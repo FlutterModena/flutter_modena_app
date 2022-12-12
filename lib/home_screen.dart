5936b1b7f1cc06fcc663728297ed574f2357c098
Elia Tolin <eliatolin@gmail.com>
2022-12-12 21:59:51 +0100

    dev

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    String osType = Platform.operatingSystem;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Modena"),
       ),
-      body: const Center(
-        child: Text("Hello world!"),
+      body: Center(
+        child: Text(
+          "Piacere sono $osType!",
+          style: const TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
